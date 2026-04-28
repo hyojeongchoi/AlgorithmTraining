0edb76121cd9ea157141cc5d8965b39460cfdaf7
hyojeongchoi <63584457+hyojeongchoi@users.noreply.github.com>
2026-04-28 23:54:14 +0900

    [level 1] Title: 신규 아이디 추천, Time: 15.75 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String solution(String new_id) {
+
+        // 1단계
+        String answer = new_id.toLowerCase();
+
+        // 2단계
+        answer = answer.replaceAll("[^a-z0-9-_.]", "");
+
+        // 3단계
+        answer = answer.replaceAll("[.]{2,}", ".");
+
+        // 4단계
+        answer = answer.replaceAll("^[.]", "");
+        answer = answer.replaceAll("[.]$", "");
+
+        // 5단계
+        if (answer.equals("")) {
+            answer = "a";
+        }
+
+        // 6단계
+        if (answer.length() > 15) {
+            answer = answer.substring(0, 15);
+        }
+        answer = answer.replaceAll("[.]$", "");
+
+        // 7단계
+        while (answer.length() < 3) {
+            answer += answer.charAt(answer.length() - 1); 
+        }
+
+        return answer;
+    }
+}
